eea32ba2c8affba90c9536a51feea97d50d00618
ishfaqmir <ishfaqmir@almuqeet.systems>
2024-05-09 17:32:04 +0530

    final fix fixed

--- app.js
+++ app.js
@@ -158,11 +158,11 @@ async function mailToHr() {
     }
   });
 
-  // "hr@almuqeet.net"
+  // 
  
     const info = await transporter.sendMail({
       from: "noreply@asmsc.net", 
-      to: "ishfaqmir@almuqeet.systems", 
+      to: "hr@almuqeet.net", 
       subject: "Greetings from the app Hr", 
       text: "", 
       html: ` <p>Hi Hr,</p>
